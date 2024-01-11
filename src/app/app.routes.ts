0dfc60acc57f7c47d111187654a7f78453417692
Cyan Lebleu <cyanlebleu@gmail.com>
2024-01-11 06:22:31 -0300

    created project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,4 +10,8 @@ export const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full',
   },
+  {
+    path: 'play-session',
+    loadComponent: () => import('./play-session/play-session.page').then( m => m.PlaySessionPage)
+  },
 ];
