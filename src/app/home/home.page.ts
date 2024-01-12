b79714ecbace5c2c8c052ed7b43c8064d5b723b8
Cyan Lebleu <cyanlebleu@gmail.com>
2024-01-11 21:40:55 -0300

    finished coding on project

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { IonContent, IonButton } from '@ionic/angular/standalone';
+import { IonContent, IonButton, IonGrid, IonRow, IonCol } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonButton, IonContent, RouterModule],
+  imports: [IonButton, IonContent, RouterModule, IonGrid, IonRow, IonCol],
 })
 export class HomePage {
   constructor() {}
